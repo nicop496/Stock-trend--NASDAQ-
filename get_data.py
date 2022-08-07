83a6ef3c8a067ae4b19eed0c16c6bd73d6694645
Nico Pujía <nicobatman0@gmail.com>
2022-08-07 17:26:35 -0300

    get_data.py improved

--- get_data.py
+++ get_data.py
@@ -1,43 +1,38 @@
-import os, re
+import os
+import re
 import pandas as pd
 
-from numpy import object_
 from time import sleep
-from datetime import datetime as dt
 from pathlib import Path
+from datetime import datetime as dt
+from numpy import object_
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.expected_conditions import element_to_be_clickable
 
 
 DATA_PATH = Path('Downloaded data')
 
 
-def _press_download_btn(url, btn_class_name, file_match_pattern, other_actions=lambda _: None):
+def download_data(url, download_btn_locator, file_match_pattern, other_actions=lambda driver: None):
     try:
-        # Go to the web page
+        caps = DesiredCapabilities.CHROME.copy()
+        caps['pageLoadStrategy'] = 'none'
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('log-level=3')
-        driver = webdriver.Chrome(service=Service('./chromedriver.exe'), options=chrome_options)
-        print('Waiting until the page loads completely...')
+        driver = webdriver.Chrome(service=Service('./chromedriver.exe'), options=chrome_options, desired_capabilities=caps)
         driver.get(url)
-
-        # Do some other optional actions (like changing the time period)
-        other_actions(driver)
-        sleep(3)
-
-        # Press the download button
-        download_btn = driver.find_element(By.CLASS_NAME, btn_class_name)
-        driver.execute_script("arguments[0].click();", download_btn)
-
+        other_actions(driver) # Do some other optional actions (like changing the time period)
+        download_btn = WebDriverWait(driver, 20).until(element_to_be_clickable(download_btn_locator))
+        driver.execute_script('arguments[0].click();', download_btn)
     except:
         print('SELENIUM ERROR')
         driver.quit()
         raise
-
     else:
         # Get and return the file path
         filepath = lambda: sorted(Path(Path.home() / 'Downloads').iterdir(), key=os.path.getmtime)[-1]
@@ -48,13 +43,14 @@ def _press_download_btn(url, btn_class_name, file_match_pattern, other_actions=l
         return filepath()
 
 
-def get_companies_list():
+def get_nasdaq_index():
     filename = f'Companies list {dt.today().strftime("%m-%Y")}.csv'
     if not filename in os.listdir(DATA_PATH):
-        filepath = _press_download_btn(
+        filepath = download_data(
             'https://www.nasdaq.com/market-activity/stocks/screener/', 
-            'ns-download-1', 
-            r'(nasdaq_screener_)[0-9]+(\.csv)$')
+            (By.CLASS_NAME, 'ns-download-1'), 
+            r'(nasdaq_screener_)[0-9]+(\.csv)$'
+        )
         os.rename(filepath, DATA_PATH / filename)
     df = pd.read_csv(DATA_PATH / filename, encoding='latin1', on_bad_lines='skip', index_col=0)
     return df[['Name', 'Country', 'Sector', 'Market Cap']]
@@ -71,19 +67,19 @@ def get_company_data(symbol:str, time_period:str):
     - 'y10' (last ten years)
     '''
     def set_time_period(driver):
-        time_period_btn = WebDriverWait(driver, 10).until(
+        time_period_btn = WebDriverWait(driver, 20).until(
             element_to_be_clickable((By.XPATH, f'//button[@data-value="{time_period}"]'))
         )
-        driver.execute_script("arguments[0].click();", time_period_btn)
+        driver.execute_script('arguments[0].click();', time_period_btn)
 
     filename = f'{symbol.upper()} {time_period.upper()} {dt.today().strftime("%d-%m-%y")}.csv'
 
     if not filename in os.listdir(DATA_PATH):
-        filepath = _press_download_btn(
-        f'https://www.nasdaq.com/market-activity/stocks/{symbol.lower()}/historical/', 
-        'historical-download', 
-        r'(HistoricalData_)[0-9]+(\.csv)$',
-        other_actions=set_time_period
+        filepath = download_data(
+            f'https://www.nasdaq.com/market-activity/stocks/{symbol.lower()}/historical/', 
+            (By.CLASS_NAME, 'historical-download'), 
+            r'(HistoricalData_)[0-9]+(\.csv)$',
+            other_actions=set_time_period
         )
         os.rename(filepath, DATA_PATH / filename)
 
